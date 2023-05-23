37c634837e7ff107ae362c29d91157fdbefda535
luvairo <luvairo@mail.ru>
2023-05-23 12:30:12 +0500

    Небольшие правки

--- GameStates/GameState.cs
+++ GameStates/GameState.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SpaceBattle.GameStates;
 
 namespace EndlessFight.GameStates
 {
